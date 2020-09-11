94f5fa1638bd0e6aa9f059a5e12d698f4c7a6542
Daniel Hart <me@danielhart.co>
2020-09-10 23:52:14 -0500

    Add font awesome and create volume controls

--- components/Player.js
+++ components/Player.js
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Track from "./Track";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause, faStop } from "@fortawesome/free-solid-svg-icons";
 
 export default function Player(props) {
   const [isLoaded, setIsLoaded] = useState(false);
   const [isPaused, setIsPaused] = useState(true);
+
+  useEffect(() => {
+    setInterval(function () {
+      if (props.song[props.song.length - 1].state() === "loaded")
+        setIsLoaded(true);
+    }, 1000);
+  }, []);
 
   function allSounds(action) {
     if (action === "play")
@@ -11,26 +21,37 @@ export default function Player(props) {
     setIsPaused(false), props.song.map((track) => track.play());
     if (action === "pause")
       setIsPaused(true), props.song.map((track) => track.pause());
-    if (action === "stop") props.song.stop();
+    if (action === "stop")
+      setIsPaused(true), props.song.map((track) => track.stop());
   }
-
-  function muteTrack(event) {}
 
   return (
     <div className='player'>
       {isPaused ? (
-        <div className='player-play button' onClick={() => allSounds("play")}>
-          &#x3e; Play
+        <div
+          className={`player-play button ${isLoaded ? "" : "disabled"}`}
+          onClick={() => allSounds("play")}
+        >
+          {isLoaded ? (
+            <span>
+              <FontAwesomeIcon icon={faPlay} /> Play
+            </span>
+          ) : (
+            <span>Loading Track</span>
+          )}
         </div>
       ) : (
         <div className='player-pause button' onClick={() => allSounds("pause")}>
-          || Pause
+          <FontAwesomeIcon icon={faPause} /> Pause
         </div>
       )}
       <div className='player-tracks'>
         {props.song.map((track, index) => (
           <Track key={index} track={{ song: track, key: index }} />
         ))}
+      </div>
+      <div className='player-stop button' onClick={() => allSounds("stop")}>
+        <FontAwesomeIcon icon={faStop} /> Stop
       </div>
     </div>
   );
--- components/Track.js
+++ components/Track.js
@@ -1,16 +1,42 @@
 import { useState } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+
 export default function Mute(props) {
   const [isUnmuted, setIsUnmuted] = useState(true);
+  const [volume, setVolume] = useState(1.0);
 
   function muteTrack(song) {
     setIsUnmuted(!isUnmuted);
     song.mute(isUnmuted);
   }
 
+  const minVolume = 0.0;
+  const maxVolume = 1.0;
+
+  function minusVolume(song) {
+    setVolume(Math.min(Math.max(Number(volume - 0.1), minVolume), maxVolume));
+    song.volume(volume);
+  }
+
+  function plusVolume(song) {
+    setVolume(Math.min(Math.max(Number(volume + 0.1), minVolume), maxVolume));
+    song.volume(volume);
+  }
+
   return (
     <div className={`player-track`}>
-      <div className='player-track-name'>Track {props.track.key}</div>
+      <div className='player-track-name'>Track {props.track.key + 1}</div>
+      <div className='player-track-volume'>
+        <div onClick={() => minusVolume(props.track.song)}>
+          <FontAwesomeIcon icon={faMinus} />
+        </div>
+        <div>Vol</div>
+        <div onClick={() => plusVolume(props.track.song)}>
+          <FontAwesomeIcon icon={faPlus} />
+        </div>
+      </div>
       <div
         className={isUnmuted ? "mute" : "mute muted"}
         onClick={() => muteTrack(props.track.song)}
